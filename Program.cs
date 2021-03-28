debcd25b40614df2a37c9411a4835acd55d5c37f
Alexander <36397871+kpebedko22@users.noreply.github.com>
2021-03-28 17:43:55 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestCollaboration {
+	class Program {
+		static void Main(string[] args) {
+
+			Console.WriteLine("Misha, hello!");
+
+			Console.ReadKey();
+		}
+	}
+}
